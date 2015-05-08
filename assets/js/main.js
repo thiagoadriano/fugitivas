e0c6b8455ced2cde9003a1e85fb628e5cd4aa492
Thiago Adriano <thiago.adriano@tivit.com.br>
2015-05-08 18:55:36 -0300

    Dados prepardos e lista carregando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,20 @@
-/// <reference path="../../typings/jquery/jquery.d.ts"/>
+
 var Fugitivas = Fugitivas || {};
 
 $(function(){
   Fugitivas.MAIN_IMAGE = $("#MainContentIMG");
   
+
   
   
   var options = {
    canEdit: true
   };
   
+
   
   
-  Fugitivas.MAIN_IMAGE.imgNotes(options);
+ // Fugitivas.MAIN_IMAGE.imgNotes(options);
   
-  Fugitivas.MAIN_IMAGE.imgNotes("addNote", 0.4,0.3,"Note <b>Text</b>");
+  
 });
--- assets/js/model.js
+++ assets/js/model.js
@@ -1,20 +1,52 @@
-/// <reference path="../../typings/knockout/knockout.d.ts"/>
-var ModelFugitivas = function(){
-	var self = this;
-	var edit = false;
-	
-	this.troggleEdit = function(){
-		if(self.edit)
-		 {
-			 alert(self.edit);
-			self.edit = false;
-		 }
-		else
-		 {
-			 alert(self.edit);
-			self.edit = true;	
-		 }
-	};
+
+var ModelFugitivas = {
+    listModel: ko.observableArray(),
+    editModel: ko.observable(),
+    dadosModal: ko.observableArray(),
+
+    openModal: function (data)
+    {
+        
+    },
+
+    editClick: function (data)
+    {
+        this.editModel(data);
+    },
+
+    saveEdit: function (data)
+    {
+
+    },
+
+    getData: function (urlLink, model)
+    {
+        $.ajax({
+            url: urlLink,
+            type: "GET",
+            dataType: "json",
+            cache: false,
+            sync: true,
+            success: function (result) {
+                for (var i in result) {
+                    model.push(ko.mapping.fromJS(result[i]));
+                }
+                
+            },
+            error: function (e) {
+                console.error(e);
+            }
+        });
+
+
+    },
+
+    init: function ()
+    {
+        this.getData("/json_dados/_lista_de_grupo.json", this.listModel);
+    }
+
 };
 
-ko.applyBindings(new ModelFugitivas());
+ko.applyBindings(ModelFugitivas);
+ModelFugitivas.init();
